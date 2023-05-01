a7279a3b197854dab2d00e9db53d60ee5f1ca670
Abdulrahman Dülek <114644911+marcusdulekilius@users.noreply.github.com>
2023-05-01 09:51:25 +0300

    Update Word Checker.py

--- Word Checker.py
+++ Word Checker.py
@@ -1,13 +1,13 @@
 text = input("Enter text: ")
 word = input("Enter the word you are looking for: ")
 words = text.split()
-word_finder = False
+word_checker = False
 for i in range(len(words)):
     if words[i] == word:
-        word_finder = True
+        word_checker = True
         break
 
-if word_finder:
+if word_checker:
     if i == 0:
         print("The word you searched for was found in the {}st word of the sentence.".format(str(i+1)))
     elif i == 1:
